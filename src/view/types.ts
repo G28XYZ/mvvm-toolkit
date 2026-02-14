b27190d2f3113bb2dec75a851e8468796a24b824
g28xyz <g28xyz@ya.ru>
2026-02-15 00:55:46 +0700

    типизация ViewFC

--- src/view/types.ts
+++ src/view/types.ts
@@ -1,13 +1,16 @@
 import { FC } from "react";
 import type { DiServices, InjectType } from "../typedi";
 
-type ResolveViewModel<T> = T extends keyof DiServices ? InjectType<T> : T;
+type ViewServiceKey = keyof DiServices & string;
+type ViewServiceKeyHint = ViewServiceKey | (string & {});
+type ViewModelArg = ViewServiceKeyHint | object;
+type ResolveViewModel<T extends ViewModelArg> = T extends ViewServiceKey ? InjectType<T> : T;
 
-export type ViewType<T> = { viewModel?: ResolveViewModel<T> };
+export type ViewType<T extends ViewModelArg = ViewServiceKeyHint> = { viewModel?: ResolveViewModel<T> };
 
-export type ViewFC<T, P extends Record<string, any> = null> = P extends null ? FC<ViewType<T>> : FC<ViewType<T> & P>
+export type ViewFC<T extends ViewModelArg = ViewServiceKeyHint, P extends Record<string, any> = null> = P extends null ? FC<ViewType<T>> : FC<ViewType<T> & P>
 
-export type ViewProps<T, P extends Record<string, any> = {}> = ViewType<T> & P;
+export type ViewProps<T extends ViewModelArg = ViewServiceKeyHint, P extends Record<string, any> = {}> = ViewType<T> & P;
 
 /**
  * Базовый класс ViewModel с методами жизненного цикла.
